ba323933f0b4f3c8fba0678ad9fb111ab899fe0f
笨兔儿 <zengqiang_1989@126.com>
2017-08-15 16:27:16 +0800

    Update ClearNgxCache.py
    支持各种场景

--- ClearNgxCache.py
+++ ClearNgxCache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf8 -*-
 
-import os
+import os, sys
 
 try:
     from hashlib import md5
@@ -11,11 +11,12 @@ except:
 
 class ClearNgxCache:
     cache_root_dir = "/data/xxx/"
-    cache_temp_dir = "/data/xxx/"
-    multi_cache_dir = ["/news/detail/"]
+    cache_temp_dir = "/data/xx/"
+    multi_cache_dir = ["/xx/xx/"]
     url = None
     clear_dir_cmd = "mv -o %s %s"
     clear_file_cmd = "rm -rf %s"
+    clear_index_dir = "index"
 
     def __init__(self, url):
         self.url = url
@@ -32,13 +33,21 @@ class ClearNgxCache:
         if not (self.multi_cache_dir is None) and len(self.multi_cache_dir) != -1:
             for temp_cache_dir in self.multi_cache_dir:
                 if str(self.url).find(temp_cache_dir) != -1:
-                    cache_dir = self.cache_root_dir + str(temp_cache_dir).strip("/").replace("/", "-")
+                    cache_dir = self.cache_root_dir + str(temp_cache_dir).strip("/").replace("/", "-") + "/"
         # 检查要清理缓存类型
         clear_type = self.check_cache_type()
         # 如果类型是目录以及是多路径的直接删除
         if not (cache_dir is None) and clear_type == "dir":
             self.clear_dir(cache_dir)
             exit(0)
+
+        # 是否是hostname/index.html页面
+        if cache_dir is None and str(self.url).split("/").__len__() == 2:
+            cache_dir = self.cache_root_dir + self.clear_index_dir + "/"
+
+        # cache_dir 证明都没有命中多路径及主页
+        if cache_dir is None:
+            cache_dir = self.cache_root_dir + str(self.url).split("/")[1] + "/"
 
         # 获取URL的md5值
         cache_dir = self.process_file(cache_dir, clear_type)
@@ -54,11 +63,13 @@ class ClearNgxCache:
     # 整站清理
     def clear_total_file(self):
         url = str(self.url)
-        if len(url.split("/")) == 1 and url[len(url) - 1] == "*":
+        if len(url.split("/")) == 2 and url[len(url) - 1] == "*":
             self.clear_dir(self.cache_root_dir)
+            exit(0)
 
     # 清理文件
     def clear_file(self, cache_file):
+        print(cache_file)
         if cache_file is None:
             print("清理文件不能为空")
             exit(0)
@@ -71,13 +82,14 @@ class ClearNgxCache:
 
     # 清理目录
     def clear_dir(self, cache_dir):
+        print(cache_dir)
         if cache_dir is None:
             print("清理目录不能为空")
             exit(0)
         if os.path.exists(cache_dir) is False:
             print("清理目录不存在")
             exit(0)
-        temp_clear_cmd = self.clear_dir_cmd % (cache_dir + "/*", self.cache_temp_dir)
+        temp_clear_cmd = self.clear_dir_cmd % (cache_dir + "*", self.cache_temp_dir)
         print("清理命令:%s" % temp_clear_cmd)
         # os.popen(temp_clear_cmd)
 
@@ -102,14 +114,13 @@ class ClearNgxCache:
         dir2 = url_md5[md5_url_len - 3:md5_url_len - 1]
         dir3 = url_md5[md5_url_len - 5:md5_url_len - 3]
         if clear_type == "dir":
-            return "%s%s/%s/%s" % (self.cache_root_dir, dir1, dir2, dir3)
+            return "%s" % cache_dir
         if clear_type == "file":
             temp_cache_dir = self.cache_root_dir
             if not (cache_dir is None):
-                temp_cache_dir = cache_dir + "/"
+                temp_cache_dir = cache_dir
             return "%s%s/%s/%s/%s" % (temp_cache_dir, dir1, dir2, dir3, url_md5)
         return None
 
-
-clearNgxCache = ClearNgxCache("m.maiche.com/news/detail/a.html")
+clearNgxCache = ClearNgxCache(sys.argv[1])
 clearNgxCache.clear_cache()
